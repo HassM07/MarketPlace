b00b980e5ff4ea0df5bd2fd1ae8366b540680309
Hassan <hassan.muhammad07@outlook.com>
2023-06-23 15:45:30 +0100

    Add basic urls and views

--- MarketPlace/views.py
+++ MarketPlace/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
 
 
 def home_view(request):
-    return HttpResponse('<h1>User Home</h1>')
-
+    return HttpResponse('<h1>Main Home</h1>')
