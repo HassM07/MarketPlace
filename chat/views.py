b83bed10cf6576f5ad1173c6bf5bafec243234f2
Hassan <hassan.muhammad07@outlook.com>
2023-07-10 17:35:27 +0100

    add chat page style

--- chat/views.py
+++ chat/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Chat
+from extendedUser.models import ExtendedUser
 # Create your views here.
 
 
 def home_view(request):
-    return render(request, 'chatHome.html', {})
+    user = ExtendedUser.objects.get(user=request.user)
+    # Chats are sorted by how long ago they were interacted with
+    chats = user.chat_set.all().order_by('-latestInteraction')
+
+    return render(request, 'chatHome.html', {'chats': chats})
+
+
+def chat_view(request, chat_id):
+    user = ExtendedUser.objects.get(user=request.user)
+    chats = user.chat_set.all().order_by('-latestInteraction')
+    chat = Chat.objects.get(id=chat_id)
+    print(chat.id)
+    return render(request, 'chatShow.html', {'chats': chats, 'mainChat': chat})
